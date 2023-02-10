caced2f1cc2d76e541a476df07bdeee4740c8ae1
Luong <luongnguyentrongduc@gmail.com>
2023-02-10 11:24:18 +0700

    test master branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited by Duc Luong Nguyen
+          TEST protected branch 
         </p>
         <a
           className="App-link"
