f408c6949a7b709bcfc2b21797d9a09506870911
Luong <luongnguyentrongduc@gmail.com>
2023-02-10 11:10:20 +0700

    test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited by Luong
+          Edited by Duc Luong Nguyen
         </p>
         <a
           className="App-link"
