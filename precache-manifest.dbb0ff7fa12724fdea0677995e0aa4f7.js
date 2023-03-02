ef48bfef69e8b7e6afeeea92573ae09e2d266b1c
luongnguyentrong <github-actions[bot]@users.noreply.github.com>
2023-03-02 13:53:19 +0000

    Updates

---
+++ precache-manifest.dbb0ff7fa12724fdea0677995e0aa4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "897a4aeabaed3f07f6f5",
+    "url": "/lvtn_client/static/css/main.13f8b065.chunk.css"
+  },
+  {
+    "revision": "897a4aeabaed3f07f6f5",
+    "url": "/lvtn_client/static/js/main.785d804c.chunk.js"
+  },
+  {
+    "revision": "b47c4def875910e2f494",
+    "url": "/lvtn_client/static/js/runtime~main.41ae4c66.js"
+  },
+  {
+    "revision": "f685a91b517b2cdfb674",
+    "url": "/lvtn_client/static/css/2.6753c3fa.chunk.css"
+  },
+  {
+    "revision": "f685a91b517b2cdfb674",
+    "url": "/lvtn_client/static/js/2.57dfdac4.chunk.js"
+  },
+  {
+    "revision": "90487a88a60ac5f4f498",
+    "url": "/lvtn_client/static/js/3.d5968772.chunk.js"
+  },
+  {
+    "revision": "88f3154da00a8e9f4e76c8f29ccbaf76",
+    "url": "/lvtn_client/index.html"
+  }
+];
