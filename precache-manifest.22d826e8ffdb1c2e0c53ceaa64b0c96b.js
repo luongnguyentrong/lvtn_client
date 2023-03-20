c6f44bc8d6aab56816a4911a5b64e4e2753f7dd0
luongnguyentrong <github-actions[bot]@users.noreply.github.com>
2023-03-20 14:49:00 +0000

    Updates

---
+++ precache-manifest.22d826e8ffdb1c2e0c53ceaa64b0c96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e59d7737b4dfa141ca67",
+    "url": "/static/css/main.6c354f9e.chunk.css"
+  },
+  {
+    "revision": "e59d7737b4dfa141ca67",
+    "url": "/static/js/main.e59d7737.chunk.js"
+  },
+  {
+    "revision": "57fb75a2e8592a1edd30",
+    "url": "/static/js/1.57fb75a2.chunk.js"
+  },
+  {
+    "revision": "8edd54820c9265375c89",
+    "url": "/static/css/2.e4e09424.chunk.css"
+  },
+  {
+    "revision": "8edd54820c9265375c89",
+    "url": "/static/js/2.8edd5482.chunk.js"
+  },
+  {
+    "revision": "221e4e16279f11513089",
+    "url": "/static/js/runtime~main.221e4e16.js"
+  },
+  {
+    "revision": "d60cf332b2ac11fb230b8bd636ebfddd",
+    "url": "/index.html"
+  }
+];
