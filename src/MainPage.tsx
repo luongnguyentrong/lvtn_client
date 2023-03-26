0e0b3e149c3b06ace449da40a8de66d5ebd266b8
Tryhard789 <khoa@LAPTOP-QVANJGBN>
2023-03-26 21:39:58 +0700

    Update user UI

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,123 +1,5 @@
-// import React from 'react';
-// import { Layout, Row, Col } from 'antd';
-// import 'antd/dist/reset.css';
-// const { Header, Footer, Content, Sider } = Layout;
-// import { Input, Button, Avatar, Breadcrumb, Menu, theme, Dropdown } from 'antd';
-// import type { MenuProps } from 'antd';
-// import { BellOutlined, UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-// import './index.css';
-// const { Search } = Input;
-// const onSearch = (value: string) => console.log(value);
-// import 'reactjs-popup/dist/index.css';
-// import { useState, useEffect } from "react";
-// import { Modal } from 'antd';
-// import CreateTable from './Create-table/CreateTable';
-// import TableList from './tablelist/tablelist';
-
-// const Main = () => {
-
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const showModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const handleOk = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   const handleCancel = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   const items: MenuProps['items'] = [
-//     {
-//       key: '1',
-//       label: (
-//        <div>uploadfile</div>
-//       ),
-//     },
-//     {
-//       key: '2',
-//       label: (
-//         <div>Tạo văn bản</div>
-//       ),
-//     },
-//     {
-//       key: '3',
-//       label: (
-//         <>
-//       <Button onClick={showModal}>
-//         Tạo bảng dữ liệu
-//       </Button>
-//       <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-//         <CreateTable />
-//       </Modal>
-//     </>
-//       ),
-//     },
-//   ];
-
-//   const {
-//     token: { colorBgContainer },
-//   } = theme.useToken();
-
-//   return (<Layout>
-//     <Header>
-//       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-//         <Col className='Logo' span={6}>
-//           <img src="./QAS.svg" alt="logo" />
-//         </Col>
-//         <Col className='Search-bar' span={12}>
-//           <Search className='Search' placeholder="input search text" onSearch={onSearch} style={{ width: 500 }} />
-//         </Col>
-//         <Col className='Bellout' span={6}>
-//           <BellOutlined className='bell' />
-//           <Avatar className='Avartar' size={40} icon={<UserOutlined />} />
-//         </Col>
-//       </Row>
-//     </Header>
-//     <Content style={{ width: '80%', height: '1000px', margin: '20px 0px' }}>
-//       <Layout>
-//         <Sider width={200} style={{ background: colorBgContainer }}>
-//           <div>
-//             <h1>Các dữ liệu quản lý</h1>
-//             <Dropdown menu={{ items }} placement="bottomLeft" arrow trigger={['click']}>
-//               <Button>+ Thêm mới</Button>
-//             </Dropdown>
-//           </div>
-//           <TableList/>
-//         </Sider>
-//         <Content style={{ width: '80%', height: '1000px', margin: '0 0' }}>
-//       <Layout>
-//         <Layout style={{ padding: '0 24px 24px' }}>
-//           <Content
-//             style={{
-//               width: 1290,
-//               padding: 24,
-//               margin: 0,
-//               minHeight: 280,
-//               background: colorBgContainer,
-//             }}
-//           >
-//             <Button style={{ margin: '0px 10px 0px 1000px' }}>Phân tích</Button>
-//             <br />
-//           </Content>
-//         </Layout>
-//       </Layout>
-//     </Content>
-//       </Layout>
-//     </Content>
-
-//     <Footer >Footer</Footer>
-
-//   </Layout>
-//   );
-// };
-
-// export default Main;
-
 import React, { useEffect, useReducer, useRef, useState } from 'react';
-import { Layout, Row, Col, Popconfirm } from 'antd';
+import { Layout, Row, Col, Popconfirm, InputNumber, Form, Typography } from 'antd';
 const { Header, Footer, Content, Sider } = Layout;
 import { Input, Button, Avatar, Breadcrumb, Menu, theme, Dropdown, Table } from 'antd';
 import type { MenuProps } from 'antd';
@@ -136,21 +18,58 @@ interface TableRow {
   [key: string]: any;
 }
 
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  editing: boolean;
+  dataIndex: string;
+  title: any;
+  inputType: 'number' | 'text';
+  record: TableRow;
+  index: number;
+  children: React.ReactNode;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{ margin: 0 }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
 
 const Main = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
+  const [form] = Form.useForm();
+  const [editingKey, setEditingKey] = useState('');
+  const showModal = () => {setIsModalOpen(true);};
+  const handleOk = () => {setIsModalOpen(false);};
+  const handleCancel = () => {setIsModalOpen(false);};
+  const cancel = () => {setEditingKey('');};
   const items: MenuProps['items'] = [
     {
       key: '1',
@@ -179,10 +98,12 @@ const Main = () => {
     },
   ];
 
+  const edit = (record: Partial<TableRow> & { key: React.Key }) => {
+    form.setFieldsValue({ id: '', col1: '', col2: '', ...record });
+    setEditingKey(record.key);
+  };
   const [data, setData] = useState<ItemType[]>([])
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const {token: { colorBgContainer },} = theme.useToken();
   const [name, setTableName] = useState<string>("");
   const [rows, setRows] = useState<TableRow[]>([]);
   const ref = useRef<TableRow[]>()
@@ -190,10 +111,36 @@ const Main = () => {
   const ref1 = useRef<string>()
   ref1.current = name
   const [columns, setColumns] = useState<TableRow[]>([]);
+  const ref2 = useRef<TableRow[]>()
+  ref2.current = columns
   const [colName, setColName] = useState([]);
   const [count, setCount] = useState(0);
   const [formData, setFormData] = useState({});
   const [colName1, setColName1] = useState([]);
+  const isEditing = (record: TableRow) => record.key === editingKey;
+  const save = async (key: React.Key) => {
+    try {
+      const row = (await form.validateFields()) as TableRow;
+
+      const newRows = [...rows];
+      const index = newRows.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newRows[index];
+        newRows.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setRows(newRows);
+        setEditingKey('');
+      } else {
+        newRows.push(row);
+        setRows(newRows);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
   const getMenuItems = async (e: any) => {
     e.preventDefault();
     let item: Array<string> = [];
@@ -240,8 +187,6 @@ const Main = () => {
     let zzz: any = {}
     zzz["key"] = count + 1;
     let i = 0;
-    // zzz["id"] = kh
-    console.log(zz)
     let z1 = [...kh, ...formValues]
     for (const ele of zz) {
       zzz[ele] = z1[i]
@@ -257,7 +202,6 @@ const Main = () => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-
 
   const handleDelete = async (key: React.Key) => {
     let k: number = Number(key) - 1
@@ -294,8 +238,6 @@ const Main = () => {
 
   };
 
-  
-
   const onClick: MenuProps['onClick'] = async (e) => {
     let column: TableRow[] = [];
     setTableName(e.key) 
@@ -310,15 +252,11 @@ const Main = () => {
       column = arr[0]
       row = arr[1]
       let k: any = {
-        title: 'operation',
-        dataIndex: 'operation',
-        fixed: 'right',
+        title: 'Delete',
+        dataIndex: 'Delete',
         width: 200,
         render: (_: any, record: { key: React.Key }) => (
           <>
-            <Button type="primary" size="small" style={{ marginRight: '20px' }}>
-              Edit
-            </Button>
             {row.length >= 1 ? (
               <Popconfirm title="Bạn có chắc chắn xóa?" onConfirm={() => handleDelete(record.key)}>
                 <a>Delete</a>
@@ -327,6 +265,31 @@ const Main = () => {
           </>
         ),
       };
+      column.push(k)
+      console.log(editingKey)
+      k = {
+        title: 'operation',
+        dataIndex: 'operation',
+        width: 200,
+        render: (_: any, record: { key: React.Key }) => (
+          <>
+            {isEditing(record) ? (
+              <span>
+                <Typography.Link onClick={() => save(record.key)} style={{ marginRight: 8 }}>
+                  Save
+                </Typography.Link>
+                <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                  <a>Cancel</a>
+                </Popconfirm>
+              </span>
+            ) : (
+              <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+                Edit
+              </Typography.Link>
+            )}
+          </>
+        ),
+      }
       column.push(k)
       setCount(row.length)
       setRows(row)
@@ -337,9 +300,22 @@ const Main = () => {
     } catch (error) {
       console.error('Failed', error);
     }
-    console.log(count)
   };
-
+  const mergedColumns = ref2.current.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: TableRow) => ({
+        record,
+        inputType: col.dataIndex === 'col1' ? 'col2' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
   return (<Layout onLoad={getMenuItems}>
     <Header>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
@@ -406,30 +382,23 @@ const Main = () => {
                   <button type="submit" className="add-row-button" onClick={handleAddData}>Add new row</button>
                 </div>
 
-                <Table columns={columns} dataSource={rows} key={count} />
-                {/* <form onSubmit={handleSubmit}>
-          {colName.map((field) => (
-            <div key={field} className="form2">
-              <label htmlFor={field}><h4>{field}</h4></label>
-              <input
-                type="text"
-                id={field}
-                name={field}
-                value={formData[field] || ""}
-                onChange={handleChange}
-              />
-            </div>
-          ))}
-          <button type="submit" className='btt2'>Add new row</button>
-        </form> */}
+                <Form form={form} component={false}><Table 
+                  components={{
+                    body: {
+                      cell: EditableCell,
+                    },
+                  }}
+                  columns={mergedColumns} 
+                  dataSource={rows} 
+                  key={count}
+                  pagination={{
+                    onChange: cancel,
+                  }}/>
+                </Form>
               </Content>
             </Layout>
           </Layout>
         </Content>
-
-        {/* <Button onClick={handleAdd} type="primary" style={{ marginBottom: 16 }}>
-          Add a row
-        </Button> */}
       </Layout>
     </Content>
 
