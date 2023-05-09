fa12f967f38350eba5fbacca414037ff9b0bf78c
Luong <luongnguyentrongduc@gmail.com>
2023-05-09 15:35:51 +0700

    add logout

--- src/pages/UnitNormal/Header/RightNav.tsx
+++ src/pages/UnitNormal/Header/RightNav.tsx
@@ -1,7 +1,13 @@
 import { Menu, MenuProps } from "antd";
 import { UserOutlined, ProfileOutlined, BellOutlined, LogoutOutlined } from '@ant-design/icons';
+import axios from "axios";
+import { getBearerHeader, getUnit } from "../../../utils";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
+    const navigate = useNavigate()
+
     const items: MenuProps['items'] = [
         {
 
@@ -21,6 +27,26 @@ export default function () {
                 {
                     key: "logout",
                     icon: <LogoutOutlined />,
+                    onClick: () => {
+                        const LogoutEndpoint = `https://sso.ducluong.monster/realms/${getUnit()}/protocol/openid-connect/logout`
+                        const config = getBearerHeader()
+
+                        if (config !== undefined) {
+                            const cookies = new Cookies()
+                            const params = new URLSearchParams()
+                            params.append("client_id", "console")
+                            params.append("refresh_token", cookies.get("refresh_token"))
+
+                            axios.post(LogoutEndpoint, params, config).then(res => {
+                                if (res.status === 204) {
+                                    cookies.remove("access_token")
+                                    cookies.remove("refresh_token")
+
+                                    navigate("/")
+                                }
+                            })
+                        } 
+                    },
                     label: "Logout"
                 }
             ]
